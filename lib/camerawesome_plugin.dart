58aa744ec7c2ca07c08960a0a49e538efbeccc0a
Vincent Mesnier <v.mesnier@apparence.io>
2023-04-03 14:49:18 +0200

    🐛 Fix permissions

--- lib/camerawesome_plugin.dart
+++ lib/camerawesome_plugin.dart
@@ -8,7 +8,6 @@ import 'package:camerawesome/src/orchestrator/models/sensor_type.dart';
 import 'package:camerawesome/src/orchestrator/models/video_options.dart';
 import 'package:camerawesome/src/web/camerawesome_web.dart';
 import 'package:collection/collection.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:universal_platform/universal_platform.dart';
